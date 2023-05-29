6b9a7d149187c42bdd4a087279eab98ab65748fd
xuerongNanopay <xuerong@nanopay.net>
2023-05-29 09:51:15 -0400

    init spring security project

---
+++ spring_security/src/main/java/xrw/springsecurity/cheatsheet/SpringSecurityCheatsheetApplication.java
@@ -0,0 +1,13 @@
+package xrw.springsecurity.cheatsheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCheatsheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCheatsheetApplication.class, args);
+	}
+
+}
---
+++ spring_security/src/test/java/xrw/springsecurity/cheatsheet/SpringSecurityCheatsheetApplicationTests.java
@@ -0,0 +1,13 @@
+package xrw.springsecurity.cheatsheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCheatsheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
